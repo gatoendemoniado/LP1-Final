632871e3d56b72a92cf8820d6d66635ff62a6366
gatoendemoniado <oirene645@gmail.com>
2025-02-22 15:22:49 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+/*document.getElementById('continuedBtn').addEventListener('click' , () => {
+    alert('Cargando la partida...');
+});*/
+
+document.getElementById('newGameBtn').addEventListener('click' , () => {
+    alert('Iniciando nueva partida..');
+});
+
+document.getElementById('loadGameBtn').addEventListener('click' , () => {
+    alert('Cargando la partida guardada...');
+});
+
+document.getElementById('challengeModeBtn').addEventListener('click' , () => {
+    alert('Iniciando modo desafio...');
+});
+
+document.getElementById('devCommentsBtn').addEventListener('click' , () => {
+    alert('Mostrando comentarios de los desarrolladores...');
+});
+
+document.getElementById('backBtn').addEventListener('click' , () => {
+    alert('Volviendo al menú principal...');
+});
